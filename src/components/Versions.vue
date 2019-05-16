46dfa74b330b382ef304640c973f358dc10f0879
Duarte David <deltaduartedavid@gmail.com>
2019-05-16 10:17:00 +0200

    Hide local version in production

--- src/components/Versions.vue
+++ src/components/Versions.vue
@@ -20,23 +20,28 @@ const localVersion = {
   url: "/vm.wasm"
 };
 
+const isProduction = process.env.NODE_ENV === "production";
+
 export default {
   props: ["value"],
   data() {
-    if (!this.value) this.$emit("input", localVersion);
+    if (!this.value && !isProduction) this.$emit("input", localVersion);
     return {
-      versions: [localVersion]
+      versions: isProduction ? [] : [localVersion]
     };
   },
   async mounted() {
     const files = await fetch(
       "https://cors-anywhere.herokuapp.com/https://api.bintray.com/packages/deltaevo/corewar/vm/files"
     ).then(res => res.json());
-    this.versions = files.map(({ version, path }) => ({
-      name: version,
-      url: `https://cors-anywhere.herokuapp.com/https://dl.bintray.com/deltaevo/corewar/${path}`
-    }));
-    this.versions.unshift(localVersion);
+    this.versions = files
+      .sort(({ created: a }, { created: b }) => Date.parse(b) - Date.parse(a))
+      .map(({ version, path }) => ({
+        name: version,
+        url: `https://cors-anywhere.herokuapp.com/https://dl.bintray.com/deltaevo/corewar/${path}`
+      }));
+    if (!isProduction) this.versions.unshift(localVersion);
+    if (!this.value) this.$emit("input", this.versions[0]);
   },
   components: {
     Multiselect
