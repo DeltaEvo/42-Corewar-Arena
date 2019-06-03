9b8b36487a345096d86edd36b10127f6f253c01f
Paul RASTOIN <prastoin@student.42.fr>
2019-06-03 14:33:33 +0200

    Add metal gradient to medals

--- src/components/WinOverlay.vue
+++ src/components/WinOverlay.vue
@@ -150,14 +150,56 @@ export default {
         }
       }
       .num {
+        -webkit-text-fill-color: transparent;
         position: absolute;
         top: -2px;
         font: small-caps bold 120px/1 sans-serif;
         left: 20px;
-        color: white;
         -webkit-text-stroke: 6px black;
+        display: inline-block;
       }
     }
+
+    > .first:nth-child(1) .num {
+        background: linear-gradient(
+          30deg,
+          #dedede,
+          #ffffff 16%,
+          #dedede 21%,
+          #ffffff 14%,
+          #454545 27%,
+          #dedede 36%,
+          #ffffff 25%,
+          #ffffff 30%,
+          #dedede 72%,
+          #ffffff 20%,
+          #dedede 84%,
+          #a1a1a1);
+      -webkit-background-clip: text;
+      }
+
+      > .first:nth-child(2) .num {
+        background: linear-gradient(
+          30deg,
+          #ca7345,
+          #ffdeca 16%,
+          #ca7345 21%,
+          #ffdeca 24%,
+          #a14521 27%,
+          #ca7345 36%,
+          #ffdeca 45%,
+          #ffdeca 60%,
+          #ca7345 72%,
+          #ffdeca 80%,
+          #ca7345 84%,
+          #732100);
+      -webkit-background-clip: text; ;
+      }
+
+      > .first:nth-child(3) .num {
+      background: white;
+      -webkit-background-clip: text;
+      }
   }
   > .winner {
     @keyframes slideDown {
@@ -192,6 +234,20 @@ export default {
           height: 126px;
         }
         > span {
+          background: linear-gradient(
+          -40deg,
+          #ffde45,
+          #ffffff 16%,
+          #ffde45 21%,
+          #ffffff 24%,
+          #452100 27%,
+          #ffde45 36%,
+          #ffffff 45%,
+          #ffffff 30%,
+          #ffde45 72%,
+          #ffffff 40%,
+          #ffde45 84%,
+          #452100);
           position: absolute;
           bottom: -20px;
           left: 0;
@@ -199,7 +255,6 @@ export default {
           display: inline-block;
           font: small-caps bold 186px/1 sans-serif;
           width: 100%;
-          background: linear-gradient(to right, #BF953F, #FCF6BA, #B38728, #FBF5B7, #AA771C);
           -webkit-background-clip: text;
           -webkit-text-fill-color: transparent;
           -webkit-text-stroke: 5px black;
